c3d7df571ebcc913b9a6fe8bdc3bab01c31c330d
Angela Alves <angelavbalves@rede.ulbra.br>
2022-08-15 15:45:43 -0300

    refactor: generic function in the service

--- popularesJava/popularesJava/Data/Service.swift
+++ popularesJava/popularesJava/Data/Service.swift
@@ -8,59 +8,74 @@
 import Foundation
 
 class Service {
-    enum RepositoryErrorState: Swift.Error {
-        case generic
-        case repositoryNotFound
-        case noConnection
-    }
-
-    enum PullRequestErrorState: Swift.Error {
-        case generic
-        case repositoryNotFound
-        case noConnection
-    }
 
     static let dispatchGroup = DispatchGroup()
     static var number = 1
-    static func getRepositories(page: Int, language: String, _ completion: @escaping (Result<RepositoryResponse, RepositoryErrorState>) -> Void) {
-        print("🛑 Request - \(number)")
-        number += 1
-        if let url = URL(string: "https://api.github.com/search/repositories?q=language:\(language)&sort=stars&order=desc&page=\(page)") {
-            let task = URLSession.shared.dataTask(with: url) { data, _, error in
+
+    static func makeRequest<T: Decodable>(endpoint: Endpoint, page: Int = 1, language: String = "", _ completion: @escaping (Result<T, ErrorState>) -> Void) {
+        if let url = makeUrlFrom(endpoint: endpoint) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, _ in
                 guard let data = data else { return }
 
-                do {
-                    let repositories = try JSONDecoder().decode(RepositoryResponse.self, from: data)
-                    completion(.success(repositories))
-                } catch {
-                    print(error)
-                    completion(.failure(.generic))
+                if let response = response as? HTTPURLResponse {
+                    switch response.statusCode {
+                        case 200...299:
+                            if let obj: T = decode(data) {
+                                completion(.success(obj))
+                            } else {
+                                completion(.failure(.generic))
+                            }
+                        case 300...399:
+                            return
+                        case 400...499:
+                            completion(.failure(.badRequest))
+                            return
+                        case 500...599:
+                            return
+                        default:
+                            return
+                    }
                 }
             }
             task.resume()
+        } else {
+            completion(.failure(.generic))
         }
     }
 
-    static func getPullRequests(username: String, repositoryTitle: String, _ completion: @escaping (Result<[PullRequestResponseItem], PullRequestErrorState>) -> Void) {
-        dispatchGroup.enter()
-        if let url = URL(string:
-            "https://api.github.com/repos/\(username)/\(repositoryTitle)/pulls")
-        {
-            print("😀 - \(url)")
-            let task = URLSession.shared.dataTask(with: url) { data, _, error in
-                guard let data = data else { return }
+    private static func makeUrlFrom(endpoint: Endpoint) -> URLRequest? {
+        guard let url = buildUrlFrom(endpoint: endpoint) else { return nil }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = endpoint.method
+        return urlRequest
+    }
 
-                do {
-                    let repositories = try JSONDecoder().decode([PullRequestResponseItem].self, from: data)
-                    completion(.success(repositories))
-                } catch {
-                    print(error)
-                    completion(.failure(.generic))
-                }
-                self.dispatchGroup.leave()
-            }
-            task.resume()
-            dispatchGroup.wait()
+    private static func buildUrlFrom(endpoint: Endpoint) -> URL? {
+        var component = URLComponents()
+        component.scheme = "https"
+        component.host = endpoint.host
+        component.path = endpoint.path
+        component.queryItems = endpoint.query
+        if !endpoint.query.isEmpty {
+            component.query = component.query?.insertString("q=")
         }
+
+        return component.url
+    }
+
+    private static func decode<T: Decodable>(_ data: Data) -> T? {
+        do {
+            let repositories = try JSONDecoder().decode(T.self, from: data)
+            return repositories
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+}
+
+extension String {
+    func insertString(_ string: String) -> String {
+        string + self
     }
 }
