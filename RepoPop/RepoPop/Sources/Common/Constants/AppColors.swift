1c77f1cfd8e0afbc8710903562e421e0053c543a
Angela Alves <angelavbalves@rede.ulbra.br>
2023-03-08 14:15:21 -0300

    feat: colors

---
+++ RepoPop/RepoPop/Sources/Common/Constants/AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  AppColors.swift
+//  RepoPop
+//
+//  Created by Angela Alves on 07/03/23.
+//
+
+import Foundation
+import UIKit
+
+struct AppColors {
+    static let lightGray = UIColor(red: 0.96, green: 0.97, blue: 0.98, alpha: 1.00)
+    static let darkGray = UIColor(red: 0.14, green: 0.16, blue: 0.18, alpha: 1.00)
+    static let red = UIColor(red: 0.84, green: 0.23, blue: 0.29, alpha: 1.00)
+    static let green = UIColor(red: 0.16, green: 0.65, blue: 0.27, alpha: 1.00)
+    static let darkGreen = UIColor(red: 0.13, green: 0.53, blue: 0.23, alpha: 1.00)
+    static let yellow = UIColor(red: 0.86, green: 0.67, blue: 0.04, alpha: 1.00)
+    static let blue = UIColor(red: 0.01, green: 0.40, blue: 0.84, alpha: 1.00)
+}
