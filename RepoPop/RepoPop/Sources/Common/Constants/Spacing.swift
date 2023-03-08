2e173b672f5e4b3888a9b4b969e840940d7e413f
Angela Alves <angelavbalves@rede.ulbra.br>
2023-03-08 14:15:40 -0300

    feat: spacing

---
+++ RepoPop/RepoPop/Sources/Common/Constants/Spacing.swift
@@ -0,0 +1,16 @@
+//
+//  Spacing.swift
+//  RepoPop
+//
+//  Created by Angela Alves on 08/03/23.
+//
+
+import Foundation
+
+struct Spacing {
+    static let extraSmall = 6.0
+    static let small = 8.0
+    static let medium = 12.0
+    static let large = 16.0
+    static let extraLarge = 20.0
+}
