68398c174a71b86a0ba799e2cefd73fcdb7decfe
Angela Alves <angelavbalves@rede.ulbra.br>
2023-03-08 14:15:32 -0300

    feat: fonts

---
+++ RepoPop/RepoPop/Sources/Common/Constants/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  RepoPop
+//
+//  Created by Angela Alves on 08/03/23.
+//
+
+import Foundation
+import UIKit
+
+struct Fonts {
+    static let title = UIFont(name: "Helvetica Neue", size: TextSize.large)
+    static let subtitle = UIFont(name: "Helvetica Neue", size: TextSize.medium)
+    static let text = UIFont(name: "Helvetica Neue", size: TextSize.small)
+    static let details = UIFont(name: "Helvetica Neue", size: TextSize.extraSmall)
+}
