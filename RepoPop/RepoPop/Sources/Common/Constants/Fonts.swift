b2ba5ddd74f860a0c9149ae8c2e95f157333413a
Angela Alves <angelavbalves@rede.ulbra.br>
2023-03-10 11:02:49 -0300

    feat: add new fonts

--- RepoPop/RepoPop/Sources/Common/Constants/Fonts.swift
+++ RepoPop/RepoPop/Sources/Common/Constants/Fonts.swift
@@ -9,8 +9,11 @@ import Foundation
 import UIKit
 
 struct Fonts {
-    static let title = UIFont(name: "Helvetica Neue", size: TextSize.large)
-    static let subtitle = UIFont(name: "Helvetica Neue", size: TextSize.medium)
+    static let title = UIFont(name: "Helvetica Neue", size: TextSize.extraLarge)
+    static let subtitleLarge = UIFont(name: "Helvetica Neue", size: TextSize.large)
+    static let subtitleMedium = UIFont(name: "Helvetica Neue", size: TextSize.medium)
     static let text = UIFont(name: "Helvetica Neue", size: TextSize.small)
     static let details = UIFont(name: "Helvetica Neue", size: TextSize.extraSmall)
+    static let logo = UIFont(name: "Baskerville", size: TextSize.extraLarge)
+    static let textField = UIFont(name: "Baskerville", size: TextSize.large)
 }
