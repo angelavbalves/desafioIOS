af8570e78ca777838d849e7a5c69a35d5830810d
Angela Alves <angelavbalves@rede.ulbra.br>
2023-03-10 16:16:58 -0300

    refactor: new font

--- RepoPop/RepoPop/Sources/Common/Constants/Fonts.swift
+++ RepoPop/RepoPop/Sources/Common/Constants/Fonts.swift
@@ -9,11 +9,9 @@ import Foundation
 import UIKit
 
 struct Fonts {
-    static let title = UIFont(name: "Helvetica Neue", size: TextSize.extraLarge)
-    static let subtitleLarge = UIFont(name: "Helvetica Neue", size: TextSize.large)
-    static let subtitleMedium = UIFont(name: "Helvetica Neue", size: TextSize.medium)
-    static let text = UIFont(name: "Helvetica Neue", size: TextSize.small)
-    static let details = UIFont(name: "Helvetica Neue", size: TextSize.extraSmall)
-    static let logo = UIFont(name: "Baskerville", size: TextSize.extraLarge)
-    static let textField = UIFont(name: "Baskerville", size: TextSize.large)
+    static let title = UIFont(name: "Baskerville", size: TextSize.extraLarge)
+    static let subtitleLarge = UIFont(name: "Baskerville", size: TextSize.large)
+    static let subtitleMedium = UIFont(name: "Baskerville", size: TextSize.medium)
+    static let text = UIFont(name: "Baskerville", size: TextSize.small)
+    static let details = UIFont(name: "Baskerville", size: TextSize.extraSmall)
 }
